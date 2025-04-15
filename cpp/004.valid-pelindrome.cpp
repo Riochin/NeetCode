0074b4619e54c9fb0faa6e37afd4efb9c1633572
りおちん <118029.ichikama@gmail.com>
2025-04-15 09:23:33 +0900

    004.valid-pelindrome

---
+++ cpp/004.valid-pelindrome.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <cctype>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        vector<char> chars;
+        for(char c :s){
+            if(isalnum(c)){
+                chars.push_back(tolower(c));
+            }
+        }
+
+        for(int i=0;i<chars.size()/2;i++){
+            if(chars[i] != chars[chars.size()-i-1]) return false;
+        }
+
+        return true;
+    }
+};
