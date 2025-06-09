323866125b6aa074a661a520657fa7a81f8ba7b2
りおちん <118029.ichikama@gmail.com>
2025-06-10 02:18:14 +0900

    014.same-binary-tree.java

---
+++ java/014.same-binary-tree.java
@@ -0,0 +1,23 @@
+package java;
+
+
+ class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if((p == null && q!= null) || (p != null && q == null)){
+            return false;
+        } else if(p == null && q == null){
+            return true;
+        }
+
+        boolean isSameVal = p.val == q.val;
+
+        return isSameVal && (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+
+    }
+
+    public class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+}
